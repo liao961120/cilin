6d538dd27b5aa1271b173954e5246eefb7b17a1a
liao <liao961120@gmail.com>
2021-12-07 12:05:48 +0800

    update

--- etc/utils.py
+++ etc/utils.py
@@ -7,7 +7,7 @@ from itertools import product, chain
 
 C = Cilin(trad=True)
 radicals = Radicals.load()
-documents = { k:list(v) for k, v in C.category_split(level=4).items() }
+documents = { k:list(v) for k, v in C.category_split(level=3).items() }
 
 with open('radical_semantic_tag.json', encoding='utf-8') as f:
     rad_sem = json.load(f)
@@ -53,23 +53,34 @@ unigramSem2id = {v:i for i, v in enumerate(id2unigramSem)}
 id2bigramSem = list(sem_bigrams)
 bigramSem2id = {v:i for i, v in enumerate(id2bigramSem)}
 
+# Indicies for a single document-term matrix
+id2semTags = list(sem_unigrams) + list(sem_bigrams)
+semTags2id = {v:i for i, v in enumerate(id2semTags)}
 
 # LDA: Encode document as bag-of-words
 def encode_doc(doc, unigram):
     data = []
+    if unigram is None:
+        semTags = [ tag for ch in chain.from_iterable(documents[doc]) for tag in unigram_sem[ch] ]
+        semTags2 = [ tag for word in documents[doc] if len(word) == 2 for tag in bigram_sem[word] ]
+        for tag, fq in Counter(semTags + semTags2).items():
+            col_idx = semTags2id[tag]
+            data.append( (col_idx, fq) )
+        return data
     if unigram:
         semTags = [ tag for ch in chain.from_iterable(documents[doc]) for tag in unigram_sem[ch] ]
         for tag, fq in Counter(semTags).items():
             col_idx = unigramSem2id[tag]
             data.append( (col_idx, fq) )
+        return data
     else:
         semTags = [ tag for word in documents[doc] if len(word) == 2 for tag in bigram_sem[word] ]
         for tag, fq in Counter(semTags).items():
             col_idx = bigramSem2id[tag]
             data.append( (col_idx, fq) )
-    return data
+        return data
 
 def get_document_topic_vec(doc, lda_model, unigram):
     bow = encode_doc(doc, unigram=unigram)
-    tp_distr = dict(lda_model.get_document_topics(bow, minimum_probability=1e-16))
+    tp_distr = dict(lda_model.get_document_topics(bow, minimum_probability=0))
     return [ tp_distr.get(i, 0.0) for i in range(lda_model.num_topics)] 
